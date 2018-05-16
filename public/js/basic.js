4938743f0132e632a7c5df2d12f227e4a9f0d1ad
xenomorphis <wachter.ma@web.de>
2018-05-16 10:04:42 +0100

    Footer-Animation
    Blend-In und Blend-Out hinzugefügt.

--- public/js/basic.js
+++ public/js/basic.js
@@ -1,13 +1,24 @@
 $(function(){
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
-    $('#nav-logo')
+	$(window).scroll(function() {
+  		if ($(document).scrollTop() > 50) {
+    		$('nav').addClass('shrink');
+    		$('#nav-logo')
                 .attr('src','/img/_logoSM.png');
-  } else {
-    $('nav').removeClass('shrink');
-    $('#nav-logo')
+  		} else {
+    		$('nav').removeClass('shrink');
+    		$('#nav-logo')
                 .attr('src','/img/_logoS.png');
-  }
-});
+  		}
+  		
+  		if($(window).scrollTop() + $(window).height() > $(document).height() - 70) {
+       	$('footer').addClass('shown');
+       	$('footer').css('visibility', 'visible');
+      } else {
+      	$('footer').removeClass('shown');
+   	}
+   	
+   	if($(window).scrollTop() + $(window).height() < $(document).height() - 100) {
+   		$('footer').css('visibility', 'hidden');
+   	}
+	});
 });
