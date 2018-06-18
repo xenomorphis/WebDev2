2ee4bbe5932a693ee495cfcfcda6f51ed8ce769b
xenomorphis <wachter.ma@web.de>
2018-06-18 15:41:56 +0100

    Shop-Page Suchfeld
    Suchfeld auf den Shopseiten hinzugefügt

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,7 +1,44 @@
 @extends('base')
 
-@section('eyecatcher')
+@section('styles')
+	hr {
+		color: #f1f707;
+		background-color: #f1f707;
+		height: 2px;
+	}
+	
+	.btn-primary {
+		background-color: rgb(242,249,135);
+		border-color: rgb(217,224,120);
+		border-radius: 5px;
+		color: rgb(46,42,136);
+		font-family: 'Source Sans Pro';
+	}
+	
+	.fulltext {
+		padding-top: 40px;	
+	}
+	
+	.search-gui {
+		margin-right: 20px;	
+	}
+	
+	#page-search > label {
+		font-size: 24px;
+		color: #f7f107;
+		text-shadow: 1px 1px #777;
+	}
+@endsection
 
+@section('eyecatcher')
+	<div class="fulltext">	
+		<form id="page-search" method="GET" action="">
+			<label for="search-field" class="highlight-text search-gui">Finde dein Event:</label>
+			<input id="search-field" class="col-md-8 search-gui" type="text" name="phrase" required>
+			<button type="submit" class="btn-md btn-primary search-gui">SUCHEN</button>
+		</form>
+	</div>
+	<hr/>
 @endsection
 
 @section('maincontent')
