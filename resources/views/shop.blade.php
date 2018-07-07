31913e40a1208adeec60cc301fe5ec5781c45431
xenomorphis <wachter.ma@web.de>
2018-07-07 12:02:15 +0100

    Showcase-Bilder
    Dummy-Images mit passenden Werbebildern (Slider) ersetzt.

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -63,17 +63,21 @@
 		<hr/>
 	@else
 		<img id="showcase-frame" src="/img/sc_frame.png">
-		<div id="showcase" class="carousel slide" data-ride="carousel" data-interval="5000">
+		<div id="showcase" class="carousel slide" data-ride="carousel" data-interval="8000">
   			<ol class="carousel-indicators">
    			<li data-target="#showcase" data-slide-to="0" class="active"></li>
     			<li data-target="#showcase" data-slide-to="1"></li>
+    			<li data-target="#showcase" data-slide-to="2"></li>
   			</ol>
   			<div class="carousel-inner">
     			<div class="carousel-item active">
-      			<img class="d-block w-100" src="/img/dummy1.jpg" alt="First slide">
+      			<img class="d-block w-100" src="/img/showcase1.jpg">
     			</div>
     			<div class="carousel-item">
-      			<img class="d-block w-100" src="/img/dummy2.jpg" alt="Second slide">
+      			<img class="d-block w-100" src="/img/showcase2.jpg">
+    			</div>
+    			<div class="carousel-item">
+      			<img class="d-block w-100" src="/img/showcase3.jpg">
     			</div>
   			</div>
 		</div>
