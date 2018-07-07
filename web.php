44299705224520ae85cc3c45baf471db49ab024d
achimGr <39114045+achimGr@users.noreply.github.com>
2018-07-07 15:11:16 +0200

    web.php und shop.blade.php abgeändert

---
+++ web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','ShopController@shop');
+Route::get('/artists','ShopController@shop');
+Route::get('/cabaret','ShopController@shop');
+Route::get('/concerts','ShopController@shop');
+Route::get('/locations','ShopController@shop');
+Route::get('/theatre','ShopController@shop');
+Route::view('/disclaimer','disclaimer');
+Route::view('/impressum','impressum');
+Auth::routes();
+
+Route::get('/search', 'ShopController@search')->name('search');
+Route::get('/admin', 'HomeController@index')->name('home');
+Route::get('/logout', 'Auth\LoginController@logout');
+
+Route::view('/admin2','admin2');
+
+Route::resource('artists', 'ArtistsController');
