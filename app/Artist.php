bef47adfdde4948691560a07ab78f354a73cb883
xenomorphis <wachter.ma@web.de>
2018-07-11 16:17:11 +0100

    Release 0.1.106esr
    Started new branch for further development.

--- app/Artist.php
+++ app/Artist.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class artist extends Model
+class Artist extends Model
 {
     //
     public function concert(){
--- app/Concert.php
+++ app/Concert.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class concert extends Model
+class Concert extends Model
 {
     //
     public function artist(){
